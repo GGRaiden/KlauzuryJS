817b52ad5fe2c07751ee1bdf4ca1d7757d646829
Maxmilián Růžička <holsek12@gmail.com>
2023-05-22 10:46:55 +0200

    Menu

--- main.js
+++ main.js
@@ -13,98 +13,96 @@ const scaledCanvas = {
 const backgroundImageHeight = 730
 
 const camera = {
-    position: {
-      x: 0,
-      y: -backgroundImageHeight + canvas.height / 2,
-    },
+  position: {
+    x: 0,
+    y: -backgroundImageHeight + canvas.height / 2,
+  },
 }
 
 const keys = {
-    d: {
-      pressed: false,
-    },
-    a: {
-      pressed: false,
-    },
-  }
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+}
 
 function drawBackground(){
-    let background = new Image();
-    background.src = "./img/background.png"
-    //ctx.fillStyle = "#373737"
-    //ctx.fillRect(0, 0, 6000, 1080);
-    ctx.drawImage(background, 1, 1, background.width, background.height, 0, 0, background.width, background.height);
+  let background = new Image();
+  background.src = "./img/backgrounds/background.png"
+  ctx.drawImage(background, 0, 0, background.width, background.height, 0, 0, background.width, background.height);
 }
 
 function draw(){
-    ctx.save();
-    ctx.scale(3, 3)
-    ctx.translate(camera.position.x, camera.position.y);
-    drawBackground();
-    level1();
-    drawPlayer();
-    ctx.restore();
+  ctx.save();
+  ctx.scale(3, 3)
+  ctx.translate(camera.position.x, camera.position.y);
+  drawBackground();
+  level1();
+  drawPlayer();
+  ctx.restore();
 
-    if (keys.d.pressed) {
-        movement.right = true;
-        movement.left = false;
-        velocity.x = 5
-        switchSprite('Run')
-        player.lastDirection = 'right'
-        shouldPanCameraToTheLeft()
-    } else if (keys.a.pressed) {
-        movement.left = true;
-        movement.right = false;
-        velocity.x = -5
-        switchSprite('RunLeft')
-        player.lastDirection = 'left'
-        shouldPanCameraToTheRight()    
-    } else if (velocity.y === 0.5) {
-      shouldPanCameraUp()
-      if (player.lastDirection === 'right') switchSprite('Idle')
-      else switchSprite('IdleLeft')
-    }
+  if (keys.d.pressed) {
+      movement.right = true;
+      movement.left = false;
+      velocity.x = 5
+      switchSprite('Run')
+      player.lastDirection = 'right'
+      shouldPanCameraToTheLeft()
+  } else if (keys.a.pressed) {
+      movement.left = true;
+      movement.right = false;
+      velocity.x = -5
+      switchSprite('RunLeft')
+      player.lastDirection = 'left'
+      shouldPanCameraToTheRight()    
+  } else if (velocity.y === 0.5) {
+    shouldPanCameraUp()
+    if (player.lastDirection === 'right') switchSprite('Idle')
+    else switchSprite('IdleLeft')
+  }
 
-    if (keys.d.pressed == false) {
-        movement.right = false;
-    } 
-    if (keys.a.pressed == false) {
-        movement.left = false;
-    }
+  if (keys.d.pressed == false) {
+      movement.right = false;
+  } 
+  if (keys.a.pressed == false) {
+      movement.left = false;
+  }
 
-    if (velocity.y < 0.5) {
-      shouldPanCameraDown()
-      if (player.lastDirection === 'right') switchSprite('Jump')
-      else switchSprite('JumpLeft')
-    } else if (velocity.y > 0.5)  {
-      shouldPanCameraUp()
-      if (player.lastDirection === 'right') switchSprite('Fall')
-      else switchSprite('FallLeft')
-    }
+  if (velocity.y < 0.5) {
+    shouldPanCameraDown()
+    if (player.lastDirection === 'right') switchSprite('Jump')
+    else switchSprite('JumpLeft')
+  } else if (velocity.y > 0.5)  {
+    shouldPanCameraUp()
+    if (player.lastDirection === 'right') switchSprite('Fall')
+    else switchSprite('FallLeft')
+  }
 }
 
 window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-      case 'd':
-        keys.d.pressed = true
-        break
-      case 'a':
-        keys.a.pressed = true
-        break
-      case ' ':
-        movement.canJump = true
-        velocity.y = -10
-        break
-    }
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      break
+    case 'a':
+      keys.a.pressed = true
+      break
+    case ' ':
+      movement.canJump = true
+      velocity.y = -10
+      break
+  }
 })
   
 window.addEventListener('keyup', (event) => {
-    switch (event.key) {
-      case 'd':
-        keys.d.pressed = false
-        break
-      case 'a':
-        keys.a.pressed = false
-        break
-    }
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
 })
---
+++ menu.js
@@ -0,0 +1,6 @@
+
+function menu() {
+    ctx.font = ("90px ThaleahFat")
+    ctx.fillStyle = "#D46A00"
+    ctx.fillText("JOURNEY OF THE KING", 359, 245);
+}
